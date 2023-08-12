095ffa56243fdbcb0c43329b941605f79228edd9
Daniel Kleyman <denny_klm1@yahoo.com>
2023-08-12 14:28:56 +0300

    HW-23

--- src/telran/time/test/DayOfWeekTest.java
+++ src/telran/time/test/DayOfWeekTest.java
@@ -2,12 +2,11 @@ package telran.time.test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import java.time.DayOfWeek;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
+import telran.time.DayOfWeek;
 import telran.time.TypeDisplay;
 
 class DayOfWeekTest {
@@ -18,37 +17,48 @@ class DayOfWeekTest {
 
 	@Test
 	@Disabled
+	
 	void test() {
-		assertEquals(0, getOrdinalDayOfWeek(DayOfWeek.MONDAY));
-		assertEquals("MONDAY", getNameDayOfWeek(DayOfWeek.MONDAY));
+		assertEquals(0, getOrdinalDayOfWeek(DayOfWeek.MON));
+		assertEquals("MON", getNameDayOfWeek(DayOfWeek.MON));
 	}
-
-	private int getOrdinalDayOfWeek(DayOfWeek day) {
+	
+	private int getOrdinalDayOfWeek(DayOfWeek day)
+	{
 		return day.ordinal();
 	}
-	private String getNameDayOfWeek(DayOfWeek day) {
+	private String getNameDayOfWeek(DayOfWeek day)
+	{
 		return day.name();
 	}
+	
 	@Test
-	void displayNameTest() {
+	void displayNameTest()
+	{
 		assertEquals("Fri", DayOfWeek.FRI.displayName(TypeDisplay.SHORT));
 		assertEquals("Friday", DayOfWeek.FRI.displayName(TypeDisplay.FULL));
 	}
+	
 	@Test
-	void displayValueTest() {
-		assertEquals(5, DayOfWeek.FRI.getValue());
+	void displayValueTest()
+	{
+		assertEquals(5,DayOfWeek.FRI.getValue());
+	}
+	
+	@Test
+	void plusDaysTest()
+	{
+		assertEquals(DayOfWeek.SAT,DayOfWeek.FRI.plusDays(22) );
 	}
 	@Test
-	void plusDaysTest() {
-		assertEquals(DayOfWeek.SAT, DayOfWeek.FRI.plusDays(22));
+	void minusDaysTest()
+	{
+		assertEquals(DayOfWeek.THU,DayOfWeek.FRI.minusDays(22) );
 	}
 	@Test
-	void minusDaysTest() {
-		assertEquals(DayOfWeek.THU, DayOfWeek.FRI.plusDays(22));
-	}
-	@Test
-	void betweenTest() {
-		assertEquals(1, DayOfWeek.FRI.between(DayOfWeek.SAT));
-		assertEquals(6, DayOfWeek.FRI.between(DayOfWeek.THU));
+	void beetweenTest()
+	{
+		assertEquals(1,DayOfWeek.FRI.berween(DayOfWeek.SAT));
+		assertEquals(6,DayOfWeek.FRI.berween(DayOfWeek.THU));
 	}
 }
