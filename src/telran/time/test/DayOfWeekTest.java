c5080c51c14803df6044d4cb1788385003340f5a
Daniel Kleyman <denny_klm1@yahoo.com>
2023-08-12 14:10:37 +0300

    HW-23

---
+++ src/telran/time/test/DayOfWeekTest.java
@@ -0,0 +1,54 @@
+package telran.time.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.time.DayOfWeek;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import telran.time.TypeDisplay;
+
+class DayOfWeekTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	@Disabled
+	void test() {
+		assertEquals(0, getOrdinalDayOfWeek(DayOfWeek.MONDAY));
+		assertEquals("MONDAY", getNameDayOfWeek(DayOfWeek.MONDAY));
+	}
+
+	private int getOrdinalDayOfWeek(DayOfWeek day) {
+		return day.ordinal();
+	}
+	private String getNameDayOfWeek(DayOfWeek day) {
+		return day.name();
+	}
+	@Test
+	void displayNameTest() {
+		assertEquals("Fri", DayOfWeek.FRI.displayName(TypeDisplay.SHORT));
+		assertEquals("Friday", DayOfWeek.FRI.displayName(TypeDisplay.FULL));
+	}
+	@Test
+	void displayValueTest() {
+		assertEquals(5, DayOfWeek.FRI.getValue());
+	}
+	@Test
+	void plusDaysTest() {
+		assertEquals(DayOfWeek.SAT, DayOfWeek.FRI.plusDays(22));
+	}
+	@Test
+	void minusDaysTest() {
+		assertEquals(DayOfWeek.THU, DayOfWeek.FRI.plusDays(22));
+	}
+	@Test
+	void betweenTest() {
+		assertEquals(1, DayOfWeek.FRI.between(DayOfWeek.SAT));
+		assertEquals(6, DayOfWeek.FRI.between(DayOfWeek.THU));
+	}
+}
